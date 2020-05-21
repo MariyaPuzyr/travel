976a762552817cd8ce68884a789f1983db80a2f1
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-21 15:25:26 +0300

    add language list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public languages = [
+    {name: 'En', flag: 'https://www.countryflags.io/US/flat/64.png'},
+    {name: 'Ru', flag: 'https://www.countryflags.io/RU/flat/64.png'},
+    {name: 'Pl', flag: 'https://www.countryflags.io/PL/flat/64.png'},
+    {name: 'Ge', flag: 'https://www.countryflags.io/be/flat/64.png'}
+  ];
 
   constructor() { }
 
