c6d1b853e6ceac43850a03f2daff70d824c233e9
beatom <project@beatom.net>
2020-05-21 12:25:39 +0300

    add created components to app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
