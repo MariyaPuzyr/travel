00f3c2e519502dc068b95f93e6e8878ed03d740f
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-22 16:47:01 +0300

    remove don't used click on dropdown from header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -13,26 +13,13 @@ export class HeaderComponent implements OnInit {
     {name: 'Pl', flag: 'https://www.countryflags.io/PL/flat/64.png'},
     {name: 'Ge', flag: 'https://www.countryflags.io/be/flat/64.png'}
   ];
-  public status = false;
-  closeResult = '';
 
   ngOnInit(): void {
-  }
-
-  /* show dropdown */
-  clickEvent() {
-    this.status = !this.status;
   }
 
   /* show modal */
   constructor(private modalService: NgbModal) {
   }
-
-  /*  open() {
-      const modalRef = this.modalService.open(SignInComponent);
-      modalRef.componentInstance.name = 'World';
-    }*/
-
 
   open(content) {
     const modalRef = this.modalService.open(content);
