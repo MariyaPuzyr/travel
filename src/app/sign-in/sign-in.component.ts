0fa47929d7dbb82b863fbbdcd1bf73b86506aff6
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-22 14:09:50 +0300

    add sign-in modal

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-
+  defaultName = 'Password';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(submittedForm) {
+    if(submittedForm.invalid) {
+      return;
+    }
+    console.log(submittedForm);
+    this.swService.addCharacter(submittedForm.value.name, submittedForm.value.side);
+  }
 }
