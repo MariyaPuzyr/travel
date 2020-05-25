00e0bb53b5f69cf27a6f14f037465d1790f07040
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-25 17:16:47 +0300

    add const for language

---
+++ src/app/main/consts/languages.const.ts
@@ -0,0 +1,6 @@
+export const Languages = [
+  {name: 'En', flag: 'https://www.countryflags.io/US/flat/64.png'},
+  {name: 'Ru', flag: 'https://www.countryflags.io/RU/flat/64.png'},
+  {name: 'Pl', flag: 'https://www.countryflags.io/PL/flat/64.png'},
+  {name: 'Ge', flag: 'https://www.countryflags.io/be/flat/64.png'}
+];
