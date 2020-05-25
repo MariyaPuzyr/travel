898532f2756a3e99b43733f346d0d00d1fa58bd1
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-25 17:17:43 +0300

    fix main modules

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,20 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
+  imports: [
+    CommonModule
+  ],
   declarations: [
-    AppComponent,
     HeaderComponent
   ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+    HeaderComponent
+  ]
 })
-export class AppModule { }
+export class MainModule { }
