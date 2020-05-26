b8a52fa531691d78497993a7132948494f787c7f
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-26 15:55:58 +0300

    add structure for login as hoteliers

--- src/app/login/views/login/login.component.ts
+++ src/app/login/views/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public phoneList = [
+    {code: '+61', flag: 'https://www.countryflags.io/US/flat/64.png'},
+    {code: '+43', flag: 'https://www.countryflags.io/US/flat/64.png'},
+    {code: '+991', flag: 'https://www.countryflags.io/US/flat/64.png'},
+    {code: '+380', flag: 'https://www.countryflags.io/US/flat/64.png'},
+    {code: '+44', flag: 'https://www.countryflags.io/US/flat/64.png'},
+  ];
 
-  constructor() { }
+  public showDropdown = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
   }
 
 }
