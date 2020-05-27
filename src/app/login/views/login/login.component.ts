fac9d763b07edc933149d8630c25344684c2940c
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-27 13:43:30 +0300

    remove code for forms from modal component

--- src/app/login/views/login/login.component.ts
+++ src/app/login/views/login/login.component.ts
@@ -6,24 +6,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  public phoneList = [
-    {code: '+61', flag: 'https://www.countryflags.io/US/flat/64.png'},
-    {code: '+43', flag: 'https://www.countryflags.io/US/flat/64.png'},
-    {code: '+991', flag: 'https://www.countryflags.io/US/flat/64.png'},
-    {code: '+380', flag: 'https://www.countryflags.io/US/flat/64.png'},
-    {code: '+44', flag: 'https://www.countryflags.io/US/flat/64.png'},
-  ];
-
-  public showDropdown = false;
-
   constructor() {
   }
 
   ngOnInit(): void {
   }
-
-  toggleDropdown() {
-    this.showDropdown = !this.showDropdown;
-  }
-
 }
