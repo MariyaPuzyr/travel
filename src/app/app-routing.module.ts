0db51f05ad5c69e77dad222a14cef881401edfc8
MariyaPuzyr <mariyapyzur@gmail.com>
2020-05-27 15:48:19 +0300

    add router for main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 
+import { MainComponent } from './main/views/main/main.component';
+
 const routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: MainComponent,
+  },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  // {path: '/', redirectTo: '/'}
+
 ];
 
 @NgModule({
