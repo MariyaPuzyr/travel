fd530e3e9c77aac3f24c2bf74909a61241b188dd
MariyaPuzyr <mariyapyzur@gmail.com>
2020-06-17 12:16:06 +0300

    remove mat-autocomplete from login form

--- src/app/login/modules/login-form/components/login-form/login-form.component.ts
+++ src/app/login/modules/login-form/components/login-form/login-form.component.ts
@@ -22,41 +22,11 @@ export class LoginFormComponent implements OnInit {
   });
   public phoneList = Phone;
 
-  stateCtrl = new FormControl();
-  filteredStates: Observable<State[]>;
-  states: State[] = [
-    {
-      name: 'Arkansas',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
-    },
-    {
-      name: 'California',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
-    },
-    {
-      name: 'Florida',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
-    },
-    {
-      name: 'Texas',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
-    }
-  ];
 
   constructor() {
 
-    this.filteredStates = this.stateCtrl.valueChanges
-      .pipe(
-        startWith(''),
-        map(state => state ? this._filterStates(state) : this.states.slice())
-      );
   }
 
-  private _filterStates(value: string): State[] {
-    const filterValue = value.toLowerCase();
-
-    return this.states.filter(state => state.name.toLowerCase().indexOf(filterValue) === 0);
-  }
   ngOnInit(): void {
   }
 
