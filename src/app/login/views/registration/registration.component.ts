67c9ead6f715da957795310eee854bc6e4da9be6
MariyaPuzyr <mariyapyzur@gmail.com>
2020-06-19 09:52:47 +0300

    registration form

--- src/app/login/views/registration/registration.component.ts
+++ src/app/login/views/registration/registration.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
+  phoneForm = new FormGroup({
+    phone: new FormControl(undefined, [Validators.required])
+  });
   constructor() { }
 
   ngOnInit(): void {
