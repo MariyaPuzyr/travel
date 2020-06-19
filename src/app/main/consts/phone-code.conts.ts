f252eea381c31d1b484bbb1a265689831b23fcb4
MariyaPuzyr <mariyapyzur@gmail.com>
2020-06-19 09:49:52 +0300

    remove phone code const

--- src/app/main/consts/phone-code.conts.ts
+++
@@ -1,7 +0,0 @@
-export const Phone = [
-  {code: '+61', flag: 'https://www.countryflags.io/US/flat/64.png'},
-  {code: '+43', flag: 'https://www.countryflags.io/US/flat/64.png'},
-  {code: '+991', flag: 'https://www.countryflags.io/US/flat/64.png'},
-  {code: '+380', flag: 'https://www.countryflags.io/US/flat/64.png'},
-  {code: '+44', flag: 'https://www.countryflags.io/US/flat/64.png'},
-];
