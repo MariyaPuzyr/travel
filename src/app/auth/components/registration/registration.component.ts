8ac4b59ddf488ff9e334f7125f6822e596075eb9
MariyaPuzyr <mariyapyzur@gmail.com>
2020-07-01 12:47:38 +0300

    add ngModel for input fields

--- src/app/auth/components/registration/registration.component.ts
+++ src/app/auth/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -10,6 +11,12 @@ export class RegistrationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
   }
 
 }
